d1d7d02debcb9c30d299ee6aceb92dbe0597a9fb
ConfidentLetter <71581547+ConfidentLetter@users.noreply.github.com>
2026-02-28 15:35:02 -0800

    added new structure

--- backend/src/ip-grabber.py
+++ backend/src/ip-grabber.py
@@ -1,6 +1,25 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
 import requests
 
 _ip = "2a09:bac2:6289:123c::1d1:d2" # temp for now. ill change it when we have a way to get the user's ip
+
+@app.route('/api/find-shelters', methods=['POST'])
+def receive_coords():
+    data = request.get_json()
+    latitude = data.get('latitude')
+    longitude = data.get('longitude')
+
+    print(f"Received coordinates: Latitude: {latitude}, Longitude: {longitude}")
+
+    return jsonify({
+            "status": "success",
+            "message": f"python got coords {latitude}, {longitude}"
+        })
 
 def get_local_coordinates(ipAddress=""):
     url = f"https://freeipapi.com/api/json/{ipAddress}"
